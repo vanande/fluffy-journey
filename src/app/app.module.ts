ceae7db26f77faed7f0dea78582f79876dba4a21
vanande <vkhatchatrian1@myges.fr>
2023-08-24 14:43:08 +0100

    update navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavComponent } from './nav/nav.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {RouterModule, RouterOutlet} from "@angular/router";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        RouterOutlet,
+        RouterModule.forRoot([
+          {path:'#', component: NavComponent}
+        ])
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
